79c10efe417edb6f546f9013e746da05ab7adbf6
Hasan Tareq <hsntareq@gmail.com>
2026-04-23 03:03:34 +0600

    fix: validate monthly plan dto

--- dist/monthly-plan/dto/monthly-plan.dto.js
+++ dist/monthly-plan/dto/monthly-plan.dto.js
@@ -1,7 +1,154 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UpsertMonthlyPlanDto = void 0;
+const class_validator_1 = require("class-validator");
 class UpsertMonthlyPlanDto {
 }
 exports.UpsertMonthlyPlanDto = UpsertMonthlyPlanDto;
+__decorate([
+    (0, class_validator_1.IsString)(),
+    __metadata("design:type", String)
+], UpsertMonthlyPlanDto.prototype, "month", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "quranStudyDays", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "haditsRead", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "literature", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "salahJamaat", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "targetContactDawah", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "targetContactWorker", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "targetContactMember", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "workerContact", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "bookDistribution", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "familyMeetingDays", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "socialWorkDays", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "orgWorkHours", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "safarDays", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "reportKeepingDays", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "selfCriticismDays", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsArray)(),
+    (0, class_validator_1.IsString)({ each: true }),
+    __metadata("design:type", Array)
+], UpsertMonthlyPlanDto.prototype, "increaseAssociate", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsArray)(),
+    (0, class_validator_1.IsString)({ each: true }),
+    __metadata("design:type", Array)
+], UpsertMonthlyPlanDto.prototype, "increaseActivist", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsArray)(),
+    (0, class_validator_1.IsString)({ each: true }),
+    __metadata("design:type", Array)
+], UpsertMonthlyPlanDto.prototype, "increaseMember", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsArray)(),
+    (0, class_validator_1.IsString)({ each: true }),
+    __metadata("design:type", Array)
+], UpsertMonthlyPlanDto.prototype, "memorizingSura", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsArray)(),
+    (0, class_validator_1.IsString)({ each: true }),
+    __metadata("design:type", Array)
+], UpsertMonthlyPlanDto.prototype, "memorizingAyat", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsArray)(),
+    (0, class_validator_1.IsString)({ each: true }),
+    __metadata("design:type", Array)
+], UpsertMonthlyPlanDto.prototype, "memorizingHadits", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "baitulmalIncreaseAmount", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], UpsertMonthlyPlanDto.prototype, "sellBooksNumber", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsArray)(),
+    (0, class_validator_1.IsString)({ each: true }),
+    __metadata("design:type", Array)
+], UpsertMonthlyPlanDto.prototype, "socialHelp", void 0);
+__decorate([
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsArray)(),
+    (0, class_validator_1.IsString)({ each: true }),
+    __metadata("design:type", Array)
+], UpsertMonthlyPlanDto.prototype, "professionalHelp", void 0);
 //# sourceMappingURL=monthly-plan.dto.js.map
--- src/monthly-plan/dto/monthly-plan.dto.ts
+++ src/monthly-plan/dto/monthly-plan.dto.ts
@@ -1,29 +1,114 @@
+import { IsArray, IsNumber, IsOptional, IsString } from 'class-validator';
+
 export class UpsertMonthlyPlanDto {
+  @IsString()
   month!: string;
+
+  @IsOptional()
+  @IsNumber()
   quranStudyDays?: number;
+
+  @IsOptional()
+  @IsNumber()
   haditsRead?: number;
+
+  @IsOptional()
+  @IsNumber()
   literature?: number;
+
+  @IsOptional()
+  @IsNumber()
   salahJamaat?: number;
+
+  @IsOptional()
+  @IsNumber()
   targetContactDawah?: number;
+
+  @IsOptional()
+  @IsNumber()
   targetContactWorker?: number;
+
+  @IsOptional()
+  @IsNumber()
   targetContactMember?: number;
+
+  @IsOptional()
+  @IsNumber()
   workerContact?: number;
+
+  @IsOptional()
+  @IsNumber()
   bookDistribution?: number;
+
+  @IsOptional()
+  @IsNumber()
   familyMeetingDays?: number;
+
+  @IsOptional()
+  @IsNumber()
   socialWorkDays?: number;
+
+  @IsOptional()
+  @IsNumber()
   orgWorkHours?: number;
+
+  @IsOptional()
+  @IsNumber()
   safarDays?: number;
+
+  @IsOptional()
+  @IsNumber()
   reportKeepingDays?: number;
+
+  @IsOptional()
+  @IsNumber()
   selfCriticismDays?: number;
 
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
   increaseAssociate?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
   increaseActivist?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
   increaseMember?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
   memorizingSura?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
   memorizingAyat?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
   memorizingHadits?: string[];
+
+  @IsOptional()
+  @IsNumber()
   baitulmalIncreaseAmount?: number;
+
+  @IsOptional()
+  @IsNumber()
   sellBooksNumber?: number;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
   socialHelp?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
   professionalHelp?: string[];
 }
