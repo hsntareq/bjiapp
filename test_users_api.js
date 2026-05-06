d1d713c6720c0284a060e088532f83cf1d1736a0
Hasan Tareq <hsntareq@gmail.com>
2026-05-06 19:18:03 +0600

    chore: remove test scripts

--- test_users_api.js
+++
@@ -1,5 +0,0 @@
-fetch('http://localhost:3001/users/by-organization?orgId=107&level=THANA', {
-  headers: { 'Authorization': 'Bearer ' + process.argv[2] }
-}).then(r => r.json()).then(data => {
-  console.log("Current Org User keys:", Object.keys(data.currentOrgUsers[0] || {}));
-});
